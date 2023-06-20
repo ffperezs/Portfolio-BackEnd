aeef024bc75a9943ae85c42d5bf76b971445b1dd
Fabian Perez Sanchez <fabian.perez92@outlook.com>
2023-06-20 01:38:21 -0300

    Se agregaron las politicas de CORS

--- src/main/java/com/portfolio/ffperezs/controller/ProyectoController.java
+++ src/main/java/com/portfolio/ffperezs/controller/ProyectoController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/proyecto")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"https://frontend-ffperezs.web.app","http://localhost:4200"})
 public class ProyectoController {
 
     private final ProyectoService proyectoService;
