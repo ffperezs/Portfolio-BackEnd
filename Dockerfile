14c125ebf96655ee4328e3b6ec1bb6b6fc32a0f3
Fabian Perez Sanchez <fabian.perez92@outlook.com>
2023-06-25 18:36:58 -0300

    corriegiendo error en Docker

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM amazoncorretto:17
 COPY target/ffperezs-0.0.1-SNAPSHOT.jar ffperezs-app.jar
 ENTRYPOINT ["java","-jar","/ffperezs-app.jar"]
+EXPOSE 8080
--- src/main/java/com/portfolio/ffperezs/security/MainSecurity.java
+++ src/main/java/com/portfolio/ffperezs/security/MainSecurity.java
@@ -50,7 +50,7 @@ public class MainSecurity {
                 .exceptionHandling().authenticationEntryPoint(jwtEntryPoint).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeHttpRequests()
-                .antMatchers("/auth/**").permitAll()
+                .antMatchers("/**").permitAll()
                 .anyRequest().authenticated();
 
         http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
